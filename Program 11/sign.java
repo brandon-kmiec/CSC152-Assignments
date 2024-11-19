c6f33b464a5543f72b96018554cc6571a890c684
Brandon Kmiec <114979976+brandon-kmiec@users.noreply.github.com>
2024-11-18 22:46:56 -0800

    Program 11 Final Commit
    Added assignment instructions
    completed sign and verify

--- Program 11/sign.java
+++ Program 11/sign.java
@@ -2,8 +2,8 @@
  * sign.java by Brandon Kmiec. Submitted for CSC 152 November 19, 2024.
  * Write a command-line program that takes arguments p, g, g^d, d, and x, and outputs
  *      r and s (all expressed as integers).
- * Followed https://www.geeksforgeeks.org/command-line-arguments-in-java/ for learning command line arguments in
- *
+ * Followed https://www.geeksforgeeks.org/command-line-arguments-in-java/ for learning command line arguments in Java
+ * Followed https://docs.oracle.com/javase/8/docs/api/?java/math/BigInteger.html for information about Java BigInteger class
  */
 
 import java.math.BigInteger;
@@ -19,7 +19,7 @@ public class sign {
 
         BigInteger p = new BigInteger(args[0]);
         BigInteger g = new BigInteger(args[1]);
-        BigInteger gd = new BigInteger(args[2]);
+        BigInteger gd = new BigInteger(args[2]); // Not sure if supposed to be used in sign
         BigInteger d = new BigInteger(args[3]);
         BigInteger x = new BigInteger(args[4]);
 
@@ -29,58 +29,60 @@ public class sign {
         Random random = new Random();
 
         BigInteger e = genRandE(random, numBits, pSub1);
-        // TODO: put in method "genRandE", remove calcGCD method
-
-
-        BigInteger eMultiInv = e.modInverse(p); // might need to be pSub1 instead of p
 
         BigInteger r = calcR(g, e, p);
-        BigInteger s = calcS(x, d, r, e, eMultiInv, pSub1);
+        BigInteger s = calcS(x, d, r, e, pSub1);
 
         System.out.println(r);
         System.out.println(s);
     } // end main
 
     /**
-     *
-     * @param g
-     * @param e
-     * @param p
-     * @return
+     * Calculate r: g^e mod p
+     * @param g generator
+     * @param e random prime
+     * @param p prime modulus
+     * @return g^e mod p
      */
     private static BigInteger calcR(BigInteger g, BigInteger e, BigInteger p) {
+        /*
         BigInteger r = g.pow(e.intValue());
         r = r.mod(p);
         return r;
+        */
+        return (g.pow(e.intValue())).mod(p);
     } // end calR
 
     /**
-     *
-     * @param x
-     * @param d
-     * @param r
-     * @param e
-     * @param eMultiInv
-     * @param pSub1
-     * @return
+     * Calculate s: (x-dr)*(e^-1) mod p-1
+     * @param x message
+     * @param d private key
+     * @param r first part of the signature
+     * @param e random prime
+     * @param pSub1 p-1 modulus
+     * @return (x-dr)*(e^-1) mod p-1
      */
-    private static BigInteger calcS(BigInteger x, BigInteger d, BigInteger r, BigInteger e, BigInteger eMultiInv, BigInteger pSub1) {
+    private static BigInteger calcS(BigInteger x, BigInteger d, BigInteger r, BigInteger e, BigInteger pSub1) {
+        /*
         BigInteger s = x.subtract(d.multiply(r));
-        s = s.multiply(eMultiInv);
+        s = s.multiply(e.modInverse(pSub1));
         s = s.mod(pSub1);
         return s;
+        */
+        return ((x.subtract(d.multiply(r))).multiply(e.modInverse(pSub1))).mod(pSub1);
     } // end calcS
 
     /**
-     *
-     * @param random
-     * @param numBits
-     * @param pSub1
-     * @return
+     * Generate a random e where 0<e<p such that gcd(e, p-1) = 1
+     * @param random random object used to generate random numbers
+     * @param numBits number of bits in p
+     * @param pSub1 p-1 modulus
+     * @return Random e: 0<e<p such that gcd(e, p-1) = 1
      */
     private static BigInteger genRandE(Random random, int numBits, BigInteger pSub1) {
         BigInteger e = BigInteger.probablePrime(numBits, random);
-        while (!e.gcd(pSub1).equals(BigInteger.valueOf(1))) {
+        while (!e.gcd(pSub1).equals(BigInteger.valueOf(1)) && e.compareTo(BigInteger.ZERO) > 0 &&
+                e.compareTo(pSub1.add(BigInteger.ONE)) < 0) {
             e = BigInteger.probablePrime(numBits, random);
         } // end while
 
--- Program 11/verify.java
+++ Program 11/verify.java
@@ -2,11 +2,67 @@
  * verify.java by Brandon Kmiec. Submitted for CSC 152 November 19, 2024.
  * Write a command-line program that takes arguments p, g, g^d, x, r, and s, and outputs
  *      1 if (r, s) is a valid signature for x and 0 otherwise.
- *
+ * Followed https://www.geeksforgeeks.org/command-line-arguments-in-java/ for learning command line arguments in Java
+ * Followed https://docs.oracle.com/javase/8/docs/api/?java/math/BigInteger.html for information about Java BigInteger class
  */
+
+import java.math.BigInteger;
 
 public class verify {
     public static void main(String[] args) {
+        if (args.length != 6) {
+            System.out.println("\nImproper Usage");
+            System.out.println("Please use: java sign <p> <g> <g^d> <x> <r> <s>");
+            System.exit(0);
+        } // end if
 
+        BigInteger p = new BigInteger(args[0]);
+        BigInteger g = new BigInteger(args[1]);
+        BigInteger gd = new BigInteger(args[2]);
+        BigInteger x = new BigInteger(args[3]);
+        BigInteger r = new BigInteger(args[4]);
+        BigInteger s = new BigInteger(args[5]);
+
+        BigInteger left = leftSide(gd, r, s, p);
+        BigInteger right = rightSide(g, x, p);
+
+        if (left.equals(right))
+            System.out.println(1);
+        else
+            System.out.println(0);
     } // end main
+
+    /**
+     * Calculate the left side: (g^d)^r * r^s mod p
+     * @param gd public key
+     * @param r first part of the signature
+     * @param s second part of the signature
+     * @param p prime modulus
+     * @return (g^d)^r * r^s mod p
+     */
+    private static BigInteger leftSide(BigInteger gd, BigInteger r, BigInteger s, BigInteger p) {
+        /*
+         BigInteger lSide = gd.pow(r.intValue());
+         lSide = lSide.multiply(r.pow(s.intValue()));
+         lSide = lSide.mod(p);
+         return lSide;
+        */
+        return (gd.pow(r.intValue()).multiply(r.pow(s.intValue()))).mod(p);
+    } // end leftSide
+
+    /**
+     * Calculate the right side: g^x mod p
+     * @param g generator
+     * @param x message
+     * @param p prime modulus
+     * @return g^x mod p
+     */
+    private static BigInteger rightSide(BigInteger g, BigInteger x, BigInteger p) {
+        /*
+        BigInteger rSide = g.pow(x.intValue());
+        rSide = rSide.mod(p);
+        return rSide;
+        */
+        return (g.pow(x.intValue())).mod(p);
+    } // end rightSide
 } // end verify
